e96590d73172e03c72e45286f142e29eba0f29f8
JAnMorss <johnanthonymorales124@gmail.com>
2025-10-21 20:41:03 +0800

    chore(infrastructure): update dependency injection and docker configuration for versioned API

--- src/TeamHub.Infrastructure/DependencyInjection.cs
+++ src/TeamHub.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Asp.Versioning;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TeamHub.Application.Abstractions;
-using TeamHub.Application.Abstractions.Caching;
 using TeamHub.Domain.ProjectMembers.Interface;
 using TeamHub.Domain.Projects.Interface;
 using TeamHub.Domain.Tasks.Interface;
 using TeamHub.Domain.Users.Interface;
 using TeamHub.Infrastructure.Authentication;
-using TeamHub.Infrastructure.Caching;
 using TeamHub.Infrastructure.Extensions;
 using TeamHub.Infrastructure.Repositories;
 using TeamHub.SharedKernel;
@@ -25,7 +24,7 @@ public static class DependencyInjection
 
         AddAuthentication(services, configuration);
 
-        //AddCaching(services, configuration);
+        AddApiVersioning(services);
 
         return services;
     }
@@ -65,13 +64,22 @@ public static class DependencyInjection
         services.AddScoped<IJwtProvider, JwtProvider>();
     }
 
-    //private static void AddCaching(IServiceCollection services, IConfiguration configuration)
-    //{
-    //    var connectionString = configuration.GetConnectionString("Cache") ??
-    //           throw new ArgumentNullException(nameof(configuration));
+    private static void AddApiVersioning(IServiceCollection services)
+    {
+        services
+            .AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+            })
+            .AddMvc()
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'V";
+                options.SubstituteApiVersionInUrl = true;
+            });
+    }
 
-    //    services.AddStackExchangeRedisCache(options => options.Configuration = connectionString);
-
-    //    services.AddSingleton<ICacheService, CacheService>();
-    //}
 }
