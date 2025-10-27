1f25e76ebcd1db2687b1af7710b8f1b752d3de96
JAnMorss <johnanthonymorales124@gmail.com>
2025-10-27 22:57:37 +0800

    refactor(user-avatar): remove old UpdateAvatar commands after refactoring avatar upload handling

--- src/TeamHub.Application/Users/Commands/UpdateAvatar/UpdateAvatarCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿using TeamHub.SharedKernel.Application.Mediator.Command;
-
-namespace TeamHub.Application.Users.Commands.UpdateAvatar;
-
-public sealed record UpdateAvatarCommand(
-    Guid UserId, 
-    string AvatarUrl) : ICommand;
