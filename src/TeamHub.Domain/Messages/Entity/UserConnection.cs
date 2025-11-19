0a62fc8224c21f46efa4c9394e5e20cc531cc1f3
JAnMorss <johnanthonymorales124@gmail.com>
2025-11-19 15:10:16 +0800

    feat(chat): add UserConnection entity for managing user chat connections

---
+++ src/TeamHub.Domain/Messages/Entity/UserConnection.cs
@@ -0,0 +1,8 @@
+﻿namespace TeamHub.Domain.Messages.Entity;
+
+public class UserConnection
+{
+    public string UserName { get; set; } = string.Empty;
+
+    public string ChatRoom { get; set; } = string.Empty;
+}
