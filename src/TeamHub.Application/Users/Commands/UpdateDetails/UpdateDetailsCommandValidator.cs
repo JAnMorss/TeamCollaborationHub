bf0084e3b809262a4825675bb9deb854d8436622
JAnMorss <johnanthonymorales124@gmail.com>
2025-11-19 15:10:12 +0800

    feat(validation): add UpdateDetails command validator

---
+++ src/TeamHub.Application/Users/Commands/UpdateDetails/UpdateDetailsCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace TeamHub.Application.Users.Commands.UpdateDetails;
+
+public sealed class UpdateDetailsCommandValidator : AbstractValidator<UpdateDetailsCommand>
+{
+    public UpdateDetailsCommandValidator()
+    {
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name cannot be empty.")
+            .MaximumLength(50).WithMessage("First name cannot exceed 50 characters.");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name cannot be empty.")
+            .MaximumLength(50).WithMessage("Last name cannot exceed 50 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email cannot be empty.")
+            .EmailAddress().WithMessage("Invalid email format.");
+
+    }
+}
