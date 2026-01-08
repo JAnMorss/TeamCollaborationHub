e096fec3a73a31e4417644e06e750e979bd17768
JAnMorss <johnanthonymorales124@gmail.com>
2026-01-08 22:03:01 +0800

    introduce user-owned repository abstraction

--- src/SharedKernel/TeamHub.SharedKernel/Repositories/IUserOwnedRepository.cs
+++ src/SharedKernel/TeamHub.SharedKernel/Repositories/IUserOwnedRepository.cs
@@ -1,4 +1,5 @@
-﻿using TeamHub.SharedKernel.Domain.Entities;
+﻿using TeamHub.SharedKernel.Application.Helpers;
+using TeamHub.SharedKernel.Domain.Entities;
 
 namespace TeamHub.SharedKernel.Repositories;
 
@@ -6,5 +7,7 @@ public interface IUserOwnedRepository<T> : IRepository<T> where T : BaseEntity, 
 {
     Task<int> CountByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
     Task<T?> GetByIdAndUserIdAsync(Guid id, Guid userId, CancellationToken cancellationToken = default);
+
+    Task<IEnumerable<T>> GetAllByUserAsync(Guid userId, QueryObject queryObject, CancellationToken cancellationToken = default);
 }
 
