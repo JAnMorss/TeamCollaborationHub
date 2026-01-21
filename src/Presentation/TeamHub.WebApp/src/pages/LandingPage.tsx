891085248fcca861c2b84cd86a2487853b6bffa5
JAnMorss <johnanthonymorales124@gmail.com>
2026-01-21 20:10:23 +0800

    Add application pages

---
+++ src/Presentation/TeamHub.WebApp/src/pages/LandingPage.tsx
@@ -0,0 +1,23 @@
+import About from "@/components/Landing/About"
+import Contact from "@/components/Landing/Contact"
+import CTA from "@/components/Landing/CTA"
+import Features from "@/components/Landing/Features"
+import Footer from "@/components/Landing/Footer"
+import Header from "@/components/Landing/Header"
+import Hero from "@/components/Landing/Hero"
+
+type Props = {}
+
+export default function LandingPage({}: Props) {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Features />
+      <About />
+      <Contact />
+      <CTA />
+      <Footer />
+    </div>
+  )
+}
