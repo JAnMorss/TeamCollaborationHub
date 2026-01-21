648af9d4fbb75650c8cb88fddac45a967a4805ba
JAnMorss <johnanthonymorales124@gmail.com>
2026-01-21 20:09:05 +0800

    Refine App component structure

--- src/Presentation/TeamHub.WebApp/src/App.tsx
+++ src/Presentation/TeamHub.WebApp/src/App.tsx
@@ -1,24 +1,11 @@
-import About from "./components/Landing/About"
-import Contact from "./components/Landing/Contact"
-import CTA from "./components/Landing/CTA"
-import Features from "./components/Landing/Features"
-import Footer from "./components/Landing/Footer"
-import Header from "./components/Landing/Header"
-import Hero from "./components/Landing/Hero"
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router";
 
 function App() {
 
   return (
     <>
-      <div >
-        <Header />
-        <Hero />
-        <Features />
-        <About />
-        <Contact />
-        <CTA />
-        <Footer />
-      </div>
+      <RouterProvider router={router} />
     </>
   )
 }
