c45ff016b18e237292497cabc53663887c65d3e5
JAnMorss <johnanthonymorales124@gmail.com>
2026-01-23 21:43:23 +0800

    Update application routing configuration

--- src/Presentation/TeamHub.WebApp/src/router/router.tsx
+++ src/Presentation/TeamHub.WebApp/src/router/router.tsx
@@ -1,6 +1,27 @@
-import LandingPage from "@/pages/LandingPage";
 import { createBrowserRouter } from "react-router-dom";
+import PublicLayout from "@/components/layout/PublicLayout";
+import AppLayout from "@/components/layout/AppLayout";
+import LandingPage from "@/pages/LandingPage";
+import Dashboard from "@/pages/dashboard";
+import PublicRoute from "@/hooks/auth/PublicRoute";
+import RequireAuth from "@/hooks/auth/RequireAuth";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <LandingPage /> },
+  {
+    element: <PublicLayout />,
+    children: [
+      { path: "/", element: <PublicRoute><LandingPage /></PublicRoute> },
+    ],
+  },
+  {
+    element: <RequireAuth />,
+    children: [
+      {
+        element: <AppLayout />,
+        children: [
+          { path: "/dashboard", element: <Dashboard /> },
+        ],
+      },
+    ],
+  },
 ]);
