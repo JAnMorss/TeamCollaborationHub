095c6a063bf697e3426e67938d610101ececc27c
JAnMorss <johnanthonymorales124@gmail.com>
2026-01-23 21:43:09 +0800

    Update application entry point

--- src/Presentation/TeamHub.WebApp/src/main.tsx
+++ src/Presentation/TeamHub.WebApp/src/main.tsx
@@ -4,14 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ThemeProvider from './components/ThemeProvider.tsx'
-
+import { AuthProvider } from './hooks/auth/useAuth'
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
-        <App />
+        <AuthProvider> 
+          <App />
+        </AuthProvider>
       </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
