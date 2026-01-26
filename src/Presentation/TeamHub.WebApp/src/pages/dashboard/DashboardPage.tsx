87933d87c13ad695387f47775c6ecf18343a787e
JAnMorss <johnanthonymorales124@gmail.com>
2026-01-26 21:57:49 +0800

    Add new DashboardPage

---
+++ src/Presentation/TeamHub.WebApp/src/pages/dashboard/DashboardPage.tsx
@@ -0,0 +1,10 @@
+import DashboardStatsCard from "@/components/dashboard/cards/DashboardStatsCard";
+
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <DashboardStatsCard />
+    </div>
+  );
+}
