5840f602a09eb603136974e18e75737a76e87d9c
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-05 16:29:51 +0800

    Update ProjectMembersHeroCard component

--- src/Presentation/TeamHub.WebApp/src/components/project-members/cards/ProjectMembersHeroCard.tsx
+++ src/Presentation/TeamHub.WebApp/src/components/project-members/cards/ProjectMembersHeroCard.tsx
@@ -3,7 +3,8 @@ import AddProjectMemberDialog from "./AddProjectMemberDialog";
 
 export default function ProjectMembersHeroCard() {
   return (
-    <Card className="bg-linear-to-r from-emerald-500 to-teal-600 text-white rounded-2xl">
+    <Card className="bg-linear-to-r from-blue-500 to-indigo-600 text-white p-6 rounded-2xl
+             dark:from-indigo-900 dark:to-indigo-800">
       <CardHeader className="space-y-3">
         <CardTitle className="text-4xl font-bold">Project Members</CardTitle>
         <CardDescription className="text-white/80 text-lg">
