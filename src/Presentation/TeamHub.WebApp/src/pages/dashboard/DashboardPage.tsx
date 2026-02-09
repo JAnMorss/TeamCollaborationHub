1a416c174dd89abf0b172271a155a5790dbd5700
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-09 21:38:46 +0800

    Update Dashboard page layout and logic

--- src/Presentation/TeamHub.WebApp/src/pages/dashboard/DashboardPage.tsx
+++ src/Presentation/TeamHub.WebApp/src/pages/dashboard/DashboardPage.tsx
@@ -5,22 +5,41 @@ import RecentActivityCard from "@/components/dashboard/cards/RecentActivityCard"
 import TaskDistributionCard from "@/components/dashboard/cards/TaskDistributionCard";
 import TaskPriorityCard from "@/components/dashboard/cards/TaskPriorityCard";
 import UpcomingDeadlinesCard from "@/components/dashboard/cards/UpcomingDeadlinesCard";
+import DashboardStatsSkeleton from "@/components/skeletons/dashboard/DashboardStatsSkeleton";
+import ProjectProgressSkeleton from "@/components/skeletons/dashboard/ProjectProgressSkeleton";
+import RecentActivitySkeleton from "@/components/skeletons/dashboard/RecentActivitySkeleton";
+import TaskDistributionSkeleton from "@/components/skeletons/dashboard/TaskDistributionSkeleton";
+import TaskPrioritySkeleton from "@/components/skeletons/dashboard/TaskPrioritySkeleton";
+import UpcomingTasksSkeleton from "@/components/skeletons/dashboard/UpcomingTasksSkeleton";
+import { Suspense } from "react";
 
 export default function DashboardPage() {
   return (
     <div className="px-6 py-8 max-w-7xl mx-auto space-y-10">
       <DashboardHeroCard />
-      <DashboardStatsCard />
+      <Suspense fallback={<DashboardStatsSkeleton />}>
+        <DashboardStatsCard />
+      </Suspense>
       
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-        <TaskDistributionCard />
-        <UpcomingDeadlinesCard />
-        <TaskPriorityCard />
+        <Suspense fallback={<TaskDistributionSkeleton />}>
+          <TaskDistributionCard />
+        </Suspense>
+        <Suspense fallback={<UpcomingTasksSkeleton />}>
+          <UpcomingDeadlinesCard />
+        </Suspense>
+        <Suspense fallback={<TaskPrioritySkeleton />}>
+          <TaskPriorityCard />
+        </Suspense>
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        <ProjectProgressCard />
-        <RecentActivityCard />
+        <Suspense fallback={<ProjectProgressSkeleton />}>
+          <ProjectProgressCard />
+        </Suspense>
+        <Suspense fallback={<RecentActivitySkeleton />}>
+          <RecentActivityCard />
+        </Suspense>
       </div>
     </div>
   );
