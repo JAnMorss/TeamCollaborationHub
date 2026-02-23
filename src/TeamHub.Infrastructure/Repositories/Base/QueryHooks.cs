80be8c8b70a0a85778532950fbad8c92921eb916
JAnMorss <johnanthonymorales124@gmail.com>
2026-02-23 20:07:39 +0800

    Enhance QueryHooks repository behavior

--- src/TeamHub.Infrastructure/Repositories/Base/QueryHooks.cs
+++ src/TeamHub.Infrastructure/Repositories/Base/QueryHooks.cs
@@ -6,17 +6,24 @@ namespace TeamHub.Infrastructure.Repositories.Base;
 
 public abstract class QueryHooks<T> where T : BaseEntity
 {
-    public virtual IQueryable<T> BuildQuery(ApplicationDbContext context, QueryObject query)
+    public virtual IQueryable<T> BuildQuery(
+        ApplicationDbContext context,
+        QueryObject query)
     {
         return context.Set<T>().AsQueryable();
     }
 
-    public virtual IQueryable<T> ApplyFilters(IQueryable<T> query, SearchQueryObject searchQueryObject, Guid? userId = null)
+    public virtual IQueryable<T> ApplyFilters(
+        IQueryable<T> query, 
+        SearchQueryObject searchQueryObject, 
+        Guid? userId = null)
     {
         return query;
     }
 
-    public virtual IQueryable<T> ApplySorting(IQueryable<T> query, QueryObject queryObject)
+    public virtual IQueryable<T> ApplySorting(
+        IQueryable<T> query, 
+        QueryObject queryObject)
     {
         if (!string.IsNullOrWhiteSpace(queryObject.SortBy))
         {
